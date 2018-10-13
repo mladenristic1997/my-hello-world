7d80e96961672cf3ebbc2e31ca7efbf45143c908
djomlaks <mladenristic1997@gmail.com>
2018-10-13 11:19:05 +0200

    Hello World in Java

---
+++ mladenristic1997/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    for(int i = 0; i < 12; i++){
+            switch(i){
+                case(0): System.out.print("H"); break;
+                case(1): System.out.print("e"); break;
+                case(2): System.out.print("l"); break;
+                case(3): System.out.print("l"); break;
+                case(4): System.out.print("o"); break;
+                case(5): System.out.print(" "); break;
+                case(6): System.out.print("W"); break;
+                case(7): System.out.print("o"); break;
+                case(8): System.out.print("r"); break;
+                case(9): System.out.print("l"); break;
+                case(10): System.out.print("d"); break;
+                case(11): System.out.print("!"); break;
+                default: System.out.println("Hello World!");            
+            }        
+        }
+    }
+}
